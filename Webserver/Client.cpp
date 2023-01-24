245852d19d541fffa1950975529b86ef6df8560f
Eduard Miclos <micloseduard@yahoo.com>
2023-01-24 14:16:05 +0200

    Webcam functionality + configurations
    Added webcam video capture functionality. Using CCTV_CAM as MACRO for the
    web camera (index 1) and FPS as MACRO for the frames per second video capture
    configuration.

--- Webserver/Client.cpp
+++ Webserver/Client.cpp
@@ -1,6 +1,6 @@
 /** 
  * --- C++ Webserver ---
- * Client-side of the client-server relationship, created for sending video frames to the central webserver.
+ * The client-side of the client-server system paradigm, created for sending video frames to the central webserver.
  * Code written by Eduard-Pavel Miclos as part of the thesis on Surveillance Systems
  * using Convolutional Neural Networks.
  *
@@ -20,8 +20,41 @@
 #include <string>
 #include <vector>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
 /* ----------------- */
 
+
+/* --- MACROs --- */
+#define CCTV_CAM 0
+#define FPS 32
+/* ------------- */
+
+
 int main() {
+
+	try {
+		cv::VideoCapture videoCapture(CCTV_CAM);
+		videoCapture.set(cv::CAP_PROP_FPS, FPS);
+
+		cv::Mat frame;
+
+		while (true) {
+			videoCapture.read(frame);
+			cv::imshow("CCTV_CAM", frame);
+
+			cv::waitKey(1);
+		}
+	}
+	catch (cv::Exception &e) {
+		const char *err_msg = e.what();
+		std::cout << "Caught exception: " << err_msg << std::endl;
+		return -1;
+	}
+
+	
 	return 0;
 }
