ab688ceb7d77731addc37bf609bb222ea01e3463
Eduard Miclos <micloseduard@yahoo.com>
2023-01-24 15:27:42 +0200

    Added optional flags
    Added optional flags (CCTV_CAM and FPS) for the Client app.
    This lets the user configure which webcam is going to be used by the current Client
    and also the FPS that the VideoCapture object is going to use. This is an useful feature,
    because different neural networks expect different inputs regarding the frames per second.

--- Webserver/Client.cpp
+++ Webserver/Client.cpp
@@ -11,6 +11,8 @@
 */
 
 /* --- Libraries --- */
+#include <string.h>
+#include <getopt.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
@@ -28,13 +30,63 @@
 /* ----------------- */
 
 
-/* --- MACROs --- */
-#define CCTV_CAM 0
-#define FPS 32
+/* --- Globals --- */
+int CCTV_CAM = 0;
+int FPS = 32;
 /* ------------- */
 
 
-int main() {
+/* This structure defines all the possible options for this program.
+ * If an option is specified, its argument is also required. */
+static struct option long_options[] = {
+	{"CCTV_CAM", 	required_argument, 	0, 	'c'},
+	{"FPS",		required_argument,	0,	'f'}
+
+};
+
+
+/* This function converts the string argument of an option to a non-negative integer.
+ * If the conversion fails, it throws an exception. */
+void read_arg(int &global_target, std::string opt, std::string optarg) {
+	try {
+		global_target = std::stoi(optarg);
+		if (global_target < 0)
+			throw std::invalid_argument("Negative integer");
+	}
+	catch (std::invalid_argument) {
+		std::cout << "Error: Invalid argument value for the " << opt << " option. Expecting non-negative integer.\n";
+		exit(EXIT_FAILURE);
+	}
+	catch (std::out_of_range) {
+		std::cout << "Error: Out of range value for the " << opt << " option.\n";
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+int main(int argc, char *argv[]) {
+	
+	int option, option_index;
+
+	/** Loops through all the options (if they exist).
+	 *  The available options are CCTV_CAM and FPS, letting the user change the webcam index and the FPS for the video capturing.
+	 *  This is useful, because different neural networks expect inputs having different FPS. */
+	while ((option = getopt_long(argc, argv, "CCTV_CAM::FPS::", long_options, &option_index)) != -1) {
+		switch(option) {
+			case 'c':
+				if (optarg)
+					read_arg(CCTV_CAM, "CCTV_CAM", optarg);				
+				break;
+			case 'f':
+				if (optarg)
+					read_arg(FPS, "FPS", optarg);
+				break;
+			default:
+				abort();
+			
+		}
+	}
+
 
 	try {
 		cv::VideoCapture videoCapture(CCTV_CAM);
