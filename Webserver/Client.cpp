cafad047d4a6f4b822ccabb7a3684cad43db81db
Eduard Miclos <micloseduard@yahoo.com>
2023-01-24 12:07:31 +0200

    Created Client.cpp and compiled. Updated .gitignore

---
+++ Webserver/Client.cpp
@@ -0,0 +1,27 @@
+/** 
+ * --- C++ Webserver ---
+ * Client-side of the client-server relationship, created for sending video frames to the central webserver.
+ * Code written by Eduard-Pavel Miclos as part of the thesis on Surveillance Systems
+ * using Convolutional Neural Networks.
+ *
+ * Politehnica University of Timisoara,
+ * 	Faculty of Automatic Control and Computer Science,
+ * 		Informatics, 3rd year
+ * ---------------------
+*/
+
+/* --- Libraries --- */
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string>
+#include <vector>
+#include <opencv2/opencv.hpp>
+/* ----------------- */
+
+int main() {
+	return 0;
+}
